1086b9d216b790c2f0b94ba72fe853b63ecb2249
Muhammmad Saqib <rooott@gmail.com>
2012-12-06 11:10:12 +0500

    init commit

--- application/controllers/ProfileController.php
+++ application/controllers/ProfileController.php
@@ -2,189 +2,194 @@
 
 class ProfileController extends Zend_Controller_Action
 {
-
     public function init()
     {
         /* Initialize action controller here */
     }
 
-    public function indexAction()
+     public function deleteAction()
+    {
+          if(Zend_Auth::getInstance()->hasIdentity())
+        {
+           $getProfiles = new Application_Model_Profile();
+           $where = "id =".$this->_request->getParam('id');
+           $userProfiles = $getProfiles->delete($where);
+           $this->_redirect('/profile/list');
+        }
+    }
+
+    public function editAction()
+    {
+         if(Zend_Auth::getInstance()->hasIdentity())
+        {
+
+        $form            = new Application_Form_Profile();
+        $profile         = new Application_Model_Profile();
+        $profilePicutres = new Application_Model_UserPics();
+        $form->getElement('submit')->setLabel('Update Profile');
+        $form->removeElement('pic_upload');
+        $form->removeElement('pic_uploadSec');
+        $form->removeElement('pic_uploadThird');
+        $form->removeElement('pic_uploadFourth');
+        $this->view->form = $form;
+            if($this->getRequest()->isPost())
+            {
+                $formData = $this->getRequest()->getPost();
+                     if ($form->isValid($formData)) 
+                    {
+                        $data = array();
+
+                        $getStates = new Application_Model_State();
+                        $state = $form->getValue('state');
+                        $stateResult = $getStates->fetchAll("state_abbr = '$state'")->toArray();
+                        $data = $form->getValues();
+                        $data['state'] = $stateResult['0']['state_id'];
+            
+                        unset($data['pic_upload']);
+                        unset($data['pic_uploadSec']);
+                        unset($data['pic_uploadThird']);
+                        unset($data['pic_uploadFourth']);
+                       
+                        $where = "id = ".$this->_request->getParam('id');
+                        $profile->update($data,$where);
+                        
+
+                        //   $where = "profile_id = ".$this->_request->getParam('id');
+                        //   $profileID = $profilePicutres->fetchRow($where)->toArray();
+                        //   $picData = array(
+                        //     $form->getValue("pic_upload"),
+                        //     $form->getValue("pic_uploadSec"),
+                        //     $form->getValue("pic_uploadThird"),
+                        //     $form->getValue("pic_uploadFourth"),
+                        //     );
+
+                        // foreach ($picData as $value) {
+                        //     if(isset($value))
+                        //     {
+                        //         $whereis = "profile_id = ".$this->_request->getParam('id');
+                        //         $updatedImages = array('picture' => $value);                          
+                        //         $profilePicutres->update($updatedImages,$whereis);
+
+                        //     }
+                            
+                        // }
+                        $this->_redirect('profile/list');
+                            
+                        
+                    }else
+                    {
+                        $form->populate($formData);
+                    }
+            }
+            else
+            {  
+                $identity = $this->_request->getParam('id');
+                if($identity > 0)
+                {
+                    $getProfileData = new Application_Model_Profile();
+                    $whereIF = "id = ".$identity;
+                        if($getProfileData->fetchRow($whereIF))
+                        {
+                            $profileData = $getProfileData->fetchRow($whereIF)->toArray();
+                            $form->populate($profileData);
+                        }
+                        else
+                        {
+                            $this->view->form = $form;
+                        }
+                }               
+            }
+        }
+    }
+
+    public function listAction()
     {
         if(Zend_Auth::getInstance()->hasIdentity())
         {
-        $form = new Application_Form_Profile();
-        $this->view->form = $form;
-        
-
-        if($this->getRequest()->isPost())
-        {
-            $formData = $this->getRequest()->getPost();
-                 if ($form->isValid($formData)) 
-                {
-                  
-                  $getStates = new Application_Model_State();
-                  $state = $form->getValue('state');
-                  $stateResult = $getStates->fetchAll("state_abbr = '$state'")->toArray();
-                  foreach ($stateResult as $value) 
-                  {
-                      $stateRow = $value['state_id']; 
-                  }
-                  
-        $data = array(
-
-                    'name'               =>  $form->getValue('name'), 
-                    'country'            =>  $form->getValue('country'),
-                    
-                    'state'              =>  $stateRow,
-                    'city'               =>  $form->getValue('city'),
-                    
-                    'zipcode'            =>  $form->getValue('zipcode'),
-                    'sex'                =>  $form->getValue('kind'),
-                    
-                    'race'               =>  $form->getValue('race'),
-                    'kind'               =>  $form->getValue('sex'),
-              
-                    'pure_bread'         =>  $form->getValue('pure_bread'),
-                    'papers'             =>  $form->getValue('papers'),
-                    
-                    'type'               =>  $form->getValue('type'),
-                    'amount'             =>  $form->getValue('amount'),
-                    
-                    'negotiable'         =>  $form->getValue('negotiable'),
-                    'details'            =>  $form->getValue('details'),
-                     
-                    'email'              =>  $form->getValue('email'),
-                    'phone'              =>  $form->getValue('phone'),
-
-                    'user_id'            =>  Zend_Auth::getInstance()->getIdentity()->id,
-                    
-                
-                     );
-
-            
-                 $InsertProfileData = new Application_Model_Profile();
-                 $id = Zend_Auth::getInstance()->getIdentity()->id;
-                 $where = "user_id = '$id'";
-                 
-                 $checkIfData = $InsertProfileData->fetchRow($where);
-                 if($checkIfData)
-                 {
-
-
-                    $InsertProfileData->update($data ,$where);
-
-                 }
-                 else
-                 {
-
-                 $InsertProfileData->insert($data);
-                 }
-                 
-
-             //  $InsertProfileData->fetchRow($where)->toArray();
-                
-/////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////// PIC upload Section////////////////////////////////////////
-                $uploadPics = new Application_Model_UserPics();
-                      
-                 if($form->pic_upload->receive())
-                {
-                    $picData = array (
-                    
-                    'profile_id'            => $id,
-                    'picture'               => $form->getValue('pic_upload'),
-                    //'date'                  => date('Y,m,d')
-                   );
-                    
-                               
-
-                }
-                
-                $uploadPics->insert($picData);
-
-                if($form->pic_uploadSec->receive())
-                {
-                    $picDataSec = array (
-                    
-                    'profile_id'            => $id,
-                    'picture'               => $form->getValue('pic_uploadSec'),
-                    //'date'              => date('Y,m,d')
-                   );
-                    
-                    
-
-                }
-                
-                $uploadPics->insert($picDataSec);
-
-                if($form->pic_uploadThird->receive())
-                {
-                    $picDataThird = array (
-                    
-                    'profile_id'       => $id,
-                    'picture'          => $form->getValue('pic_uploadThird'),
-                    //'date'                  => date('Y,m,d')
-                   );
-                    
-                    
-
-                }
-                
-                $uploadPics->insert($picDataThird);
-
-                if($form->pic_uploadFourth->receive())
-                {
-                    $picDataFourth = array (
-                    
-                    'profile_id'            => $id,
-                    'picture'      => $form->getValue('pic_uploadFourth'),
-                    //'date'              => date('Y,m,d')
-                   );
-                    
-               
-                   
-
-                }
-                
-                $uploadPics->insert($picDataFourth);
-/////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////// PIC upload Section End////////////////////////////////////////
-                }
-                 else
-               {
-                $form->populate($formData);
-               }
-           }
-           
-           else
-           {  
-             $identity = Zend_Auth::getInstance()->getIdentity()->id; 
-             if($identity > 0)
-
-             {
-                $getProfileData = new Application_Model_Profile();
-                $whereIF = "user_id = ".$identity;
-
-                if($getProfileData->fetchRow($whereIF))
-                {
-                $profileData = $getProfileData->fetchRow($whereIF)->toArray();
-                $form->populate($profileData);
-                }
-                else
-                {
-                    $this->view->form = $form;
-                }
-             }               
-           }
-          
-        
-
-        
+           /// sending profile details to list page../////
+           $getProfiles = new Application_Model_Profile();
+           $where = "user_id =".Zend_Auth::getInstance()->getIdentity()->id;
+           $userProfiles = $getProfiles->fetchAll($where);
+           $this->view->userProfiles = $userProfiles;
         }
-        else 
-            {
-               $this->_redirect('index');
-            }
+        
     }
 
- }
+
+    public function addAction()
+    {
+       if(Zend_Auth::getInstance()->hasIdentity())
+        {
+
+        $form            = new Application_Form_Profile();
+        $profile         = new Application_Model_Profile();
+        $profilePicutres = new Application_Model_UserPics();
+        $form->getElement('submit')->setLabel('Add Profile');
+        $this->view->form = $form;
+            if($this->getRequest()->isPost())
+            {
+                $formData = $this->getRequest()->getPost();
+                     if ($form->isValid($formData)) 
+                    {
+                        $data = array();
+
+                        $getStates = new Application_Model_State();
+                        $state = $form->getValue('state');
+                        $stateResult = $getStates->fetchAll("state_abbr = '$state'")->toArray();
+                        $data = $form->getValues();
+                        $data['user_id'] =  Zend_Auth::getInstance()->getIdentity()->id;
+                        $data['state'] = $stateResult['0']['state_id'];
+            $picnumber =$data['picture'] = rand();
+
+                        unset($data['pic_upload']);
+                        unset($data['pic_uploadSec']);
+                        unset($data['pic_uploadThird']);
+                        unset($data['pic_uploadFourth']);
+                       
+                        if($profile->insert($data))
+                        {
+                          $where = "picture = ".$picnumber;
+                          $profileID = $profile->fetchRow($where)->toArray();
+                          $picData = array(
+                            $form->getValue("pic_upload"),
+                            $form->getValue("pic_uploadSec"),
+                            $form->getValue("pic_uploadThird"),
+                            $form->getValue("pic_uploadFourth"),
+                            );
+
+                          foreach ($picData as $value) {
+                            if(isset($value))
+                            {
+                                $updatedImages = array(
+                                    'picture'    => $value,
+                                    'profile_id' => $profileID['id'],
+                                    );                          
+                               $profilePicutres->insert($updatedImages);
+                            }
+                            
+                        }
+                        
+                        $this->_redirect('profile/list');
+                        }
+                                
+                    }
+               
+                         
+              }
+      
+          }
+        }
+            
+            
+    
+    }
+
+   
+
+
+
+
+
+
+
+
--- application/controllers/SignUpController.php
+++ application/controllers/SignUpController.php
@@ -78,8 +78,8 @@ class SignUpController extends Zend_Controller_Action
        $smtpConfigs = array(
             
             'auth'          =>      'login',
-            'username'      =>      'nayatelorg',
-            'password'      =>      'quickbrown123',
+            'username'      =>      'nayab.rajpoot',
+            'password'      =>      'jeerry1979',
             'ssl'           =>      'ssl',
             'port'          =>       465
             
